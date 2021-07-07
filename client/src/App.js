268c23fada22b3710173536e4abe7618938e31ad
andhikayusup <andeeka.ym@gmail.com>
2021-07-07 13:10:50 +0700

    Implement react components and containers

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'semantic-ui-react'
+
+import 'semantic-ui-css/semantic.min.css'
 import './App.css'
 
-function App() {
+import Navbar from './components/Navbar'
+import Home from './containers/Home'
+import Login from './containers/Login'
+import Register from './containers/Register'
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <Router>
+      <Container>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   )
 }
 
